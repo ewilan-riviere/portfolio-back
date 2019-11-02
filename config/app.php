76f2a6421da1e56e93980520272d98bff816fc61
ewiefairy <ewiefairy@gmail.com>
2019-11-02 09:27:48 +0100

    Add cors

--- config/app.php
+++ config/app.php
@@ -169,6 +169,8 @@ return [
          * Package Service Providers...
          */
 
+        Barryvdh\Debugbar\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -228,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
