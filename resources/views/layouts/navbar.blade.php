23e99461e43b3a1785b9e5fafb5c824233b12b07
EwieFairy <ewiefairy@gmail.com>
2019-12-21 17:54:52 +0100

    Update for selfhost

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -8,12 +8,12 @@
     <nav class="white no-shadows">
         <div class="nav-wrapper">
             <a href="{{ route('welcome') }}" class="brand-logo">
-                <img src="{{ asset('logo.png') }}" alt="" class="logo-shadowed navbar-logo">
+                <img src="{{ asset('images/laravel.svg') }}" alt="" class="navbar-logo">
             </a>
             <a href="#" data-target="mobile-demo" class="sidenav-trigger">
                 <i class="material-icons">menu</i>
             </a>
-            
+
             <ul class="right hide-on-med-and-down">
                 @foreach ($navbar as $item)
                     @php
@@ -21,7 +21,7 @@
                     @endphp
                     <li class="{{$activeForeach}}">
                         <a href="{{ route($item->link) }}">
-                            <i class="material-icons left">{{$item->icon}}</i> 
+                            <i class="material-icons left">{{$item->icon}}</i>
                             {{ __('navigation.'.$item->title) }}
                         </a>
                     </li>
@@ -31,7 +31,7 @@
                         <a class="dropdown-trigger {{($route == 'home') ? 'active' : ''}} {{($route == 'login') ? 'active' : ''}} {{($route == 'register') ? 'active' : ''}}"
                             data-target="auth-dropdown"
                         >
-                            <i class="material-icons left">account_circle</i> 
+                            <i class="material-icons left">account_circle</i>
                                 @if (Auth::check())
                                     {{ Auth::user()->name }}
                                 @else
@@ -42,15 +42,15 @@
                     </li>
                 @endif
             </ul>
-            
+
         </div>
     </nav>
-    
+
     <ul id="mobile-demo" class="sidenav">
         <li class="black-text">
             <div class="user-view">
                 <div class="background grey lighten-4">
-                    
+
                 </div>
                 <a href="#user">
                     <img class="circle" src="{{asset('logo.png')}}">
@@ -73,7 +73,7 @@
             @endphp
             <li class="{{$active}}">
                 <a href="{{ route($value->link) }}">
-                    <i class="material-icons left">{{$value->icon}}</i> 
+                    <i class="material-icons left">{{$value->icon}}</i>
                     {{$value->title}}
                 </a>
             </li>
