c53261ad6cacc8015d3d2315b3b533f43a9f5a2f
Ewilan Rivière <ewilan.riviere@gmail.com>
2020-12-17 09:42:23 +0100

    fix formations project light

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -15,30 +15,45 @@ class ProjectResource extends JsonResource
      */
     public function toArray($request)
     {
-        $skills = SkillResource::collection($this->skills);
-        $skills->map(function ($i) { $i->light = true; });
+        if ($this->light) {
+            $resource = [
+                'slug'                                => $this->slug,
+                'title'                               => $this->title,
+                'order'                               => $this->order,
+                'resume'                              => $this->resume,
+                'assets'                              => [
+                    'image'                               => getImage($this->image, true),
+                    'imageTitle'                          => getImage($this->image_title, true),
+                ],
+            ];
+        } else {
+            $skills = SkillResource::collection($this->skills);
+            $skills->map(function ($i) { $i->light = true; });
 
-        $developers = DeveloperResource::collection($this->developers);
-        $developers->map(function ($i) { $i->light = true; });
+            $developers = DeveloperResource::collection($this->developers);
+            $developers->map(function ($i) { $i->light = true; });
 
-        return [
-            'slug'                                => $this->slug,
-            'title'                               => $this->title,
-            'order'                               => $this->order,
-            'resume'                              => $this->resume,
-            'assets'                              => [
-                'image'                               => getImage($this->image, true),
-                'imageTitle'                          => getImage($this->image_title, true),
-                'font'                                => getPath($this->font, true),
-            ],
-            'links'                               => [
-                'github'                               => $this->link_github,
-                'project'                              => $this->link_project,
-            ],
-            'status'     => $this->status,
-            'created_at' => $this->created_at,
-            'skills'     => $skills,
-            'developers' => $developers,
-        ];
+            $resource = [
+                'slug'                                => $this->slug,
+                'title'                               => $this->title,
+                'order'                               => $this->order,
+                'resume'                              => $this->resume,
+                'assets'                              => [
+                    'image'                               => getImage($this->image, true),
+                    'imageTitle'                          => getImage($this->image_title, true),
+                    'font'                                => getPath($this->font, true),
+                ],
+                'links'                               => [
+                    'github'                               => $this->link_github,
+                    'project'                              => $this->link_project,
+                ],
+                'status'     => $this->status,
+                'created_at' => $this->created_at,
+                'skills'     => $skills,
+                'developers' => $developers,
+            ];
+        }
+
+        return $resource;
     }
 }
