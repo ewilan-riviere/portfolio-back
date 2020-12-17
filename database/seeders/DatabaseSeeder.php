9c571038ee49618fd617148653602089e2c6d520
Ewilan Rivière <ewilan.riviere@gmail.com>
2020-12-17 14:27:37 +0100

    ✨ review seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use File;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,13 +22,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        rrmdir(storage_path('app/public/*'));
-        $this->directoryToStorage('documents');
-        $this->directoryToStorage('projects');
-        $this->directoryToStorage('icons');
-        $this->directoryToStorage('formations');
-        $this->directoryToStorage('fonts');
-        $this->directoryToStorage('skills');
+        $dirs = ['cache',
+            'documents',
+            'projects',
+            'icons',
+            'formations',
+            'fonts',
+            'skills',
+        ];
+        $clean = File::cleanDirectory(storage_path('app/public/'));
+        $clean = $clean ? 'OK' : 'Error';
+        dump("Clean storage: $clean");
+        foreach ($dirs as $key => $dir) {
+            $this->directoryToStorage($dir);
+        }
         // $this->call(UsersTableSeeder::class);
         $this->call(SkillsCategoriesTableSeeder::class);
         $this->call(FormationsSeeder::class);
