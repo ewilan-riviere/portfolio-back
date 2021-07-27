8701ec69c727b3da675c5b8e1164a6f340c935f9
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-07-27 10:26:18 +0200

    fix

--- app/Providers/ImageProvider.php
+++ app/Providers/ImageProvider.php
@@ -17,43 +17,48 @@ class ImageProvider
      */
     public static function simple_color_thief($img, $default = 'eee')
     {
-        if (@exif_imagetype($img)) { // CHECK IF IT IS AN IMAGE
-            $type = getimagesize($img)[2]; // GET TYPE
-            if (1 === $type) { // GIF
-                $image = imagecreatefromgif($img);
-                // IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
-                if (127 == imagecolorsforindex($image, imagecolorstotal($image) - 1)['alpha']) {
-                    return '000';
+        try {
+            if (@exif_imagetype($img)) { // CHECK IF IT IS AN IMAGE
+                $type = getimagesize($img)[2]; // GET TYPE
+                if (1 === $type) { // GIF
+                    $image = imagecreatefromgif($img);
+                    // IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
+                    if (127 == imagecolorsforindex($image, imagecolorstotal($image) - 1)['alpha']) {
+                        return '000';
+                    }
+                } elseif (2 === $type) { // JPG
+                    $image = imagecreatefromjpeg($img);
+                } elseif (3 === $type) { // PNG
+                    $image = imagecreatefrompng($img);
+                    // IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
+                    if ((imagecolorat($image, 0, 0) >> 24) & 0x7F === 127) {
+                        return '000';
+                    }
+                } elseif (18 === $type) { // WEBP
+                    $image = imagecreatefromwebp($img);
+                    // IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
+                    if ((imagecolorat($image, 0, 0) >> 24) & 0x7F === 127) {
+                        return '000';
+                    }
+                } else { // NO CORRECT IMAGE TYPE (GIF, JPG or PNG)
+                    return $default;
                 }
-            } elseif (2 === $type) { // JPG
-                $image = imagecreatefromjpeg($img);
-            } elseif (3 === $type) { // PNG
-                $image = imagecreatefrompng($img);
-                // IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
-                if ((imagecolorat($image, 0, 0) >> 24) & 0x7F === 127) {
-                    return '000';
-                }
-            } elseif (18 === $type) { // WEBP
-                $image = imagecreatefromwebp($img);
-                // IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
-                if ((imagecolorat($image, 0, 0) >> 24) & 0x7F === 127) {
-                    return '000';
-                }
-            } else { // NO CORRECT IMAGE TYPE (GIF, JPG or PNG)
+            } else { // NOT AN IMAGE
                 return $default;
             }
-        } else { // NOT AN IMAGE
-            return $default;
-        }
 
-        $newImg = imagecreatetruecolor(1, 1); // FIND DOMINANT COLOR
-        imagecopyresampled($newImg, $image, 0, 0, 0, 0, 1, 1, imagesx($image), imagesy($image));
-        $hexa_color = dechex(imagecolorat($newImg, 0, 0));
-        if (! self::is_hex($hexa_color)) {
-            $hexa_color = $default;
-        }
+            $newImg = imagecreatetruecolor(1, 1); // FIND DOMINANT COLOR
+            imagecopyresampled($newImg, $image, 0, 0, 0, 0, 1, 1, imagesx($image), imagesy($image));
+            $hexa_color = dechex(imagecolorat($newImg, 0, 0));
+            if (! self::is_hex($hexa_color)) {
+                $hexa_color = $default;
+            }
 
-        return $hexa_color; // RETURN HEX COLOR
+            return $hexa_color; // RETURN HEX COLOR
+        } catch (\Throwable $th) {
+            //throw $th;
+            echo $img;
+        }
     }
 
     public static function is_hex($hex_code)
